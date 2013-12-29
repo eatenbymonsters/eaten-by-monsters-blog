922be1aa5c2d24c6eef90f0e02208f14cdc8d590
Tom Hazledine <tom@eatenbymonsters.com>
2013-12-29 14:58:40 +0000

    began module01 slide one layout

--- themes/ebm-03-0/module-featured.php
+++ themes/ebm-03-0/module-featured.php
@@ -1,1 +1,48 @@
-<h4>slide one:reviews by date</h4>
+<div class="featured-loop">
+  
+  <?php
+  // Show only the most recent post with Meta_Data Key of 'featured'
+  $featured_args = array(
+    'posts_per_page' => 1,
+    'meta_query' => array(
+      array(
+        'key' => 'featured'//,// Filter by Value as well as Key
+        //'value' => 'yes'
+      )
+    )
+  );
+  $featured_posts = new WP_Query($featured_args);
+  
+  if($featured_posts->have_posts()) : 
+    while($featured_posts->have_posts()) : 
+      $featured_posts->the_post();
+  ?>
+  
+    <!-- Shown if there is a featured post: -->
+    <div class="featured-post" style="background-image: url(
+      <?php
+        $postheadimg = get_post_meta($post->ID, 'post-head-img', true);
+        if ($postheadimg) {
+  		    echo get_post_meta($post->ID, 'post-head-img', true);
+  	    }else{ ?>
+		      http://eatenbymonsters.com/wp-content/uploads/2013/12/default-thumb-small.png
+		  <?php } ?>
+		)">
+			
+    </div><!-- .featured-post -->
+
+  <?php
+    endwhile;
+  else: 
+  ?>
+  
+    <!-- Shown if there isn't a featured post: -->
+    <div class="featured-post">
+      <h1>No Featured Post</h1>
+    </div><!-- .featured-post -->
+    
+  <?php
+  endif;
+  wp_reset_postdata();
+  ?>
+</div><!-- .featured-loop -->
