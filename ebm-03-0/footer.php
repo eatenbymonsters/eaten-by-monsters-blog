51d486daea4678af302c70b4fa61341377d4cbb7
Tom Hazledine <tom@eatenbymonsters.com>
2014-02-20 14:55:42 +0000

    Tidied up footer

--- ebm-03-0/footer.php
+++ ebm-03-0/footer.php
@@ -5,6 +5,28 @@
 	  'theme_location' => 'footer_menu',
 	  'menu_class' => 'foot-nav clearfix'
 	)); ?>
+	
+	<ul class="footer-icon-links clearfix">
+    <li class="header-link link-unfocus">
+      <a class="rss-link" title="Soundcloud" href="https://soundcloud.com/eaten-by-monsters-blog/">
+        <span aria-hidden="true" data-icon="&#xe004;"></span>
+        <span class="visuallyhidden">Soundcloud</span>
+      </a>
+    </li>
+    <li class="header-link link-unfocus">
+      <a class="rss-link" title="Twitter" href="http://twitter.com/eatenbymonsters/">
+        <span aria-hidden="true" data-icon="&#xe001;"></span>
+        <span class="visuallyhidden">Twitter</span>
+      </a>
+    </li>
+    <li class="header-link link-unfocus">
+      <a class="rss-link" title="RSS" href="/feed/">
+        <span aria-hidden="true" data-icon="&#xe006;"></span>
+        <span class="visuallyhidden">RSS</span>
+      </a>
+    </li>
+  </ul><!-- .header-links -->
+  
 	<?php get_template_part('foot'); ?>
 </footer><!-- .main-footer -->
 
